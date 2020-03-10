8b28047effdbab9fb39ac35b531d04f0f2704b2c
Zora Fuchs <>
2020-03-10 10:51:21 +0100

    Inital Commit

---
+++ BlazorApp2/Server/Controllers/TennisMatchController.cs
@@ -0,0 +1,22 @@
+﻿using BlazorApp2.Shared;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorApp2.Server.Controllers
+{
+    [ApiController]
+    [Route("TennisMatch")]
+    public class TennisMatchController : ControllerBase
+    {
+        [HttpGet("Get")]
+        public void Get()
+        {
+            // TODO: Implement the Get Method. Return type is "void" now however you should change this depending of what type you are going to return.
+        }
+
+
+
+    }
+
+
+}
---
+++ BlazorApp2/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,31 @@
+﻿using BlazorApp2.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorApp2.Server.Controllers
+{
+    [ApiController]
+    [Route("WeatherForecast")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [HttpGet("Get")]
+        public List<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToList();
+        }
+    }
+}
