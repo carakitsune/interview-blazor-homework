0ec560ab34e86189032facda94f88fea980d4738
Zora Fuchs <>
2020-03-10 15:41:52 +0100

    callculate game by advantige sets, but it seems to be played with tiebreak

--- BlazorApp2/Server/Controllers/TennisMatchController.cs
+++ BlazorApp2/Server/Controllers/TennisMatchController.cs
@@ -9,8 +9,45 @@ namespace BlazorApp2.Server.Controllers
         [HttpGet("Get")]
         public string Get()
         {
-            var wins = System.IO.File.ReadAllText("./TestData/test.csv");
-            return wins;
+            var winData = System.IO.File.ReadAllText("./TestData/test.csv");
+            string[] wins = winData.Split(',');
+
+            var playerAWinsCount = 0;
+            var playerBWinsCount = 0;
+
+            var playerAGameCount = 0;
+            var playerBGameCount = 0;
+
+            var playerAAdvantageSetCount = 0;
+            var playerBAdvantageSetCount = 0;
+
+            foreach (var win in wins)
+            {
+                if (win == "1") {
+                    playerAWinsCount++;
+                    if (playerAWinsCount > 3 && (playerAWinsCount - playerBWinsCount) > 1) {
+                        playerAGameCount ++;
+                        playerAWinsCount = 0;
+
+                        if (playerAGameCount > 5 && (playerAGameCount - playerBGameCount) > 1) {
+                            playerAAdvantageSetCount ++;
+                            playerAGameCount = 0;
+                        }
+                    }
+                } else {
+                    playerBWinsCount ++;
+                    if (playerBWinsCount > 3 && (playerBWinsCount - playerAWinsCount) > 1) {
+                        playerBGameCount ++;
+                        playerBWinsCount = 0;
+
+                        if (playerBGameCount > 5 && (playerBGameCount - playerAGameCount) > 1) {
+                            playerBAdvantageSetCount ++;
+                            playerBGameCount = 0;
+                        }
+                    }
+                }
+            }
+            return $"Player A has {playerAAdvantageSetCount} Sets, {playerAGameCount} Games and {playerAWinsCount} Wins, Player B has {playerBAdvantageSetCount} Sets, {playerBGameCount} Games and {playerBWinsCount} Wins";
             // TODO: Implement the Get Method. Return type is "void" now however you should change this depending of what type you are going to return.
         }
     }
